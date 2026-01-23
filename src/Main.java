c8e6f05e9b0a1cec8dba131abeded39d38b30a5f
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-23 09:32:27 +0100

    Ejecicio sobre una agenda de contactos con un Map y un Set implementados con un bucle

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, Set<String>> contactos = new HashMap<>();
+
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Introduce el contacto (fin para terminar): ");
+            String contacto = sc.nextLine();
+
+            if (contacto.equalsIgnoreCase("fin")) {
+                continuar = false;
+            } else if (contactos.containsKey(contacto) ) {
+                System.out.println("El contacto ya existe");
+                System.out.println("Introduce el contacto (fin para terminar): ");
+                contacto = sc.nextLine();
+            }else{
+                Set<String> telefonos = new HashSet<>();
+                boolean continuar2 = true;
+
+                while (continuar2) {
+                    System.out.println("Introduce el número de teléfono (fin para terminar): ");
+                    String telefono = sc.nextLine();
+
+                    if (telefono.equalsIgnoreCase("fin")) {
+                        continuar2 = false;
+                    } else {
+                        telefonos.add(telefono);
+                    }
+                }
+
+                contactos.putIfAbsent(contacto, telefonos);
+            }
+        }
+        // Mostrar la agenda completa
+        System.out.println("AGENDA:");
+        for (Map.Entry<String, Set<String>> entry : contactos.entrySet()) {
+            System.out.println("Contacto: " + entry.getKey() +
+                    " | Teléfonos: " + entry.getValue());
+        }
+
+        sc.close();
+    }
 }
